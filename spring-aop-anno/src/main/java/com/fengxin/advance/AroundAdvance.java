b0c5530efffc42a901aff220e7c7f63455b56ffe
枫 <fx20031215@163.com>
2024-08-15 17:46:08 +0800

    feat: 添加注释

--- spring-aop-anno/src/main/java/com/fengxin/advance/AroundAdvance.java
+++ spring-aop-anno/src/main/java/com/fengxin/advance/AroundAdvance.java
@@ -6,6 +6,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 /**
  * @author FENGXIN
  * @date 2024/7/28
@@ -21,18 +23,23 @@ import org.springframework.stereotype.Component;
 public class AroundAdvance {
     // 使用@Around注解标明环绕通知方法
     @Around ("com.fengxin.pointcut.MyPointCut.myPointCut1()")
-    public Object around(ProceedingJoinPoint proceedingJoinPoint){
+    public Object around(
+            // 通过在通知方法形参位置声明ProceedingJoinPoint类型的形参，
+            // Spring会将这个类型的对象传给我们
+            ProceedingJoinPoint proceedingJoinPoint){
         // 获取方法参数 保证目标方法被执行即可
         Object[] args = proceedingJoinPoint.getArgs ();
         Object result;
         try {
             // 增强代码
-            System.out.println ("环绕开始");
+            System.out.println ("环绕开始" + Arrays.toString (args));
             // 执行目标方法
+            // 过ProceedingJoinPoint对象调用目标方法
+            // 目标方法的返回值一定要返回给外界调用者
             result = proceedingJoinPoint.proceed (args);
-            System.out.println ("环绕结束");
+            System.out.println ("环绕结束" + result);
         }catch (Throwable e){
-            System.out.println ("事物回滚");
+            System.out.println ("事物回滚" + e.getMessage());
             // 必须再抛出异常 否则调用者拿不到异常
             throw new RuntimeException (e);
         }
